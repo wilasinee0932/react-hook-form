0a209bc05358b146c009b80f84bf9818f4ad2d1d
wilasinee0932 <wilasinee.su.66@ubu.ac.th>
2023-08-15 14:57:54 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState();
   return (
     <Container>
       <h4>React Hook Form</h4>
-      <Form>
+      <Form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
         <Form.Group className="mb-3">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            {...register("email")}
+            type="email"
+            placeholder="Enter email"
+          />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password" />
+          <Form.Control
+            {...register("password")}
+            type="password"
+            placeholder="Enter password"
+          />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>ประเภทสมาชิก</Form.Label>
-          <Form.Select>
+          <Form.Select {...register("userType")}>
             <option value="1" selected>
               ทั่วไป
             </option>
@@ -25,7 +37,14 @@ function App() {
           </Form.Select>
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Check type="checkbok" label="Remember me" />
+          <Form.Check
+            {...register("remember")}
+            type="checkbox"
+            label="Remember me"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Text>{data}</Form.Text>
         </Form.Group>
         <Button type="submit">Submit</Button>
       </Form>
