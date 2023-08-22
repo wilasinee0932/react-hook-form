a86f54ab2a820afd9d91e93cc0a6ad9b1a8d687c
wilasinee0932 <wilasinee.su.66@ubu.ac.th>
2023-08-22 10:04:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { Button, Container, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [data, setData] = useState();
   return (
     <Container>
@@ -13,18 +17,34 @@ function App() {
         <Form.Group className="mb-3">
           <Form.Label>Email address</Form.Label>
           <Form.Control
-            {...register("email")}
+            {...register("email", {
+              required: "required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "รูปแบบของอีเมลไม่ถูกต้ัอง",
+              },
+            })}
             type="email"
             placeholder="Enter email"
           />
+          <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            {...register("password")}
+            {...register("password", {
+              required: "required",
+              minLength: {
+                value: 8,
+                message: "รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษรขึ้นไป",
+              },
+            })}
             type="password"
             placeholder="Enter password"
           />
+          <Form.Text className="text-danger">
+            {errors.password?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>ประเภทสมาชิก</Form.Label>
